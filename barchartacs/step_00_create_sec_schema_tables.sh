79ef5498f35df35798b126b18be90730d12023e8
henry perlman <bperlman1@gmail.com>
2020-02-13 20:31:07 -0500

    step_00_create_sec_schema_tables.sh

--- barchartacs/step_00_create_sec_schema_tables.sh
+++ barchartacs/step_00_create_sec_schema_tables.sh
@@ -1,10 +1,10 @@
 # run build_db.py to build the sec_schema schema and the tables options_table and futures_table
 #
-# example with postgres username and password and default virtual env
-# $ bash step_00_create_tables.sh db_username db_password 
+# example with virtualenv=~/Virtualenvs3/dashrisk3, db config_name=secdb_aws test mode (don't create tables)
+# $ bash step_00_create_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws test 
 #
-# example with postgres username and password and specify virtual env as myvirtual_env
-# $ bash step_00_create_tables.sh db_username db_password myvirtual_env 
+# example with virtualenv=~/Virtualenvs3/dashrisk3, db config_name=secdb_aws live mode (CREATE tables)
+# $ bash step_00_create_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws live 
 #
 
 virtualenv_folder=${1}
@@ -14,7 +14,7 @@ testorlive=${3}
 
 if [[ -z ${virtualenv_folder} ]]
 then
-    virtualenv_folder="~/Virtualenvs3/dashrisk2"
+    virtualenv_folder="~/Virtualenvs3/dashrisk3"
 fi
 
 
