5737331948e191b92c9fe69a34e1388f2461f420
henry perlman <bperlman1@gmail.com>
2020-02-13 20:35:42 -0500

    step_00_create_sec_schema_tables.sh

--- barchartacs/step_00_create_sec_schema_tables.sh
+++ barchartacs/step_00_create_sec_schema_tables.sh
@@ -1,10 +1,10 @@
 # run build_db.py to build the sec_schema schema and the tables options_table and futures_table
 #
 # example with virtualenv=~/Virtualenvs3/dashrisk3, db config_name=secdb_aws test mode (don't create tables)
-# $ bash step_00_create_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws test 
+# $ bash step_00_create_sec_schema_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws test 
 #
 # example with virtualenv=~/Virtualenvs3/dashrisk3, db config_name=secdb_aws live mode (CREATE tables)
-# $ bash step_00_create_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws live 
+# $ bash step_00_create_sec_schema_tables.sh ~/Virtualenvs3/dashrisk3 secdb_aws live 
 #
 
 virtualenv_folder=${1}
