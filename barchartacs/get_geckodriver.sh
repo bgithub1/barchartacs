5b5b2383c9c82d29c86140c32b635de45f5d1bce
henry perlman <bperlman1@gmail.com>
2020-02-15 08:28:16 -0500

    step_01_download_monthly_acs_files.py

--- barchartacs/get_geckodriver.sh
+++ barchartacs/get_geckodriver.sh
@@ -1,9 +1,26 @@
-# fetch geckodriver, untar it and move it to either ../ or /usr/local/bin
-firefox_dir="$1"
+# fetch geckodriver, untar it and move it to either ../ 
+# fetch firefox, untar it and move the folder to /opt and create a symlink to /usr/bin/firefox
 
+firefox_dir="$1"
+gecko_version=$2
+firefox_version=$3
+
+# check for firefox directory
 if [[ -z ${firefox_dir} ]]
 then
     firefox_dir='/opt'
+fi
+
+# check for geckodriver version
+if [[ -z ${gecko_version} ]]
+then
+    gecko_version='0.26.0'
+fi
+
+# check for firefox version
+if [[ -z ${firefox_version} ]]
+then
+    firefox_version='57.0'
 fi
 
 
@@ -14,7 +31,7 @@ rm gecko*
 rm firefox*
 
 # get geckodriver
-wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v${gecko_version}/geckodriver-v${gecko_version}-linux64.tar.gz
 tar -xvzf geckodriver*
 chmod +x geckodriver
 echo you must now move the geckodriver to either the root of this project or to /usr/local/bin
@@ -22,7 +39,7 @@ echo sudo cp geckodriver /usr/local/bin/
 cp -n geckodriver ../
 
 # get version 57.0 of firefox
-wget http://ftp.mozilla.org/pub/firefox/releases/57.0/linux-$(uname -m)/en-US/firefox-57.0.tar.bz2
+wget http://ftp.mozilla.org/pub/firefox/releases/${firefox_version}/linux-$(uname -m)/en-US/firefox-${firefox_version}.tar.bz2
 tar -xjf firefox-57.0.tar.bz2
 sudo mv -vn firefox "${firefox_dir}"
 sudo ln -s "${firefox_dir}/firefox/firefox" /usr/bin/firefox
