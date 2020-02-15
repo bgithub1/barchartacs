23ba076e07ce5274107fad6317af6e063b97e132
henry perlman <bperlman1@gmail.com>
2020-02-14 23:37:43 -0500

    get_geckodriver.sh

--- barchartacs/get_geckodriver.sh
+++ barchartacs/get_geckodriver.sh
@@ -9,6 +9,9 @@ fi
 
 
 cd drivers
+# clear out dir
+rm gecko*
+rm firefox*
 
 # get geckodriver
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
