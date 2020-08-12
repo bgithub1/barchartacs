a0f73844cb93463e66a71b2a9ed8702331afa401
henry perlman <bperlman1@gmail.com>
2020-08-12 16:29:48 -0400

    add ./ and ../ to sys.path in dashapp_csvaccess_index.py

--- barchartacs/dashapp_csvaccess_index.py
+++ barchartacs/dashapp_csvaccess_index.py
@@ -3,6 +3,10 @@ Created on Aug 9, 2020
 
 @author: bperlman1
 '''
+import sys,os
+sys.path.append(os.path.abspath('./'))
+sys.path.append(os.path.abspath('../'))
+
 from dashapp_db_table_access import dcc,html,Output,Input#@UnResolvedImport
 from dashapp_csvaccess_app import app,app_port,url_base_pathname#@UnResolvedImport
 from dashapp_csvaccess_zipaccess_app import app_layout as ziplayout#@UnResolvedImport
